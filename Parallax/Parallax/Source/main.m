dbf9952f8f2271509f1f25cfd7f61de760b72922
Rene Cacheaux <rene.cacheaux@mutualmobile.com>
2013-03-30 13:13:35 -0500

    Fresh XCode Project.

---
+++ Parallax/Parallax/Source/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Parallax
+//
+//  Created by Rene Cacheaux on 3/30/13.
+//  Copyright (c) 2013 RCach. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PXAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([PXAppDelegate class]));
+  }
+}
---
+++ Parallax/ParallaxTests/ParallaxTests.h
@@ -0,0 +1,13 @@
+//
+//  ParallaxTests.h
+//  ParallaxTests
+//
+//  Created by Rene Cacheaux on 3/30/13.
+//  Copyright (c) 2013 RCach. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ParallaxTests : SenTestCase
+
+@end
